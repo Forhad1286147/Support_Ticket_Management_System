0b0e07358f9b66c4fc82288d11b622cb06e31175
Forhad1286147 <forhad121398@gmail.com>
2026-08-20 18:47:54 +0600

    NotificationRepository

---
+++ backend/src/Support_Ticket_Management_System_Infrastucture/Repositories/NotificationRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Support_Ticket.Application.Common.Interfaces.IRepositories;
+using Support_Ticket.Domain.Entities;
+using Support_Ticket.Infrastucture.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Support_Ticket.Infrastucture.Repositories
+{
+    public class NotificationRepository:INotificationRepository
+    {
+        private readonly AppDbContext _context;
+        public NotificationRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Notification> AddAsync(Notification notification)
+        {
+            try
+            {
+                await _context.Notifications.AddAsync(notification);
+                await _context.SaveChangesAsync();
+                return notification;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var existingNotification = await _context.Notifications.FindAsync(id);
+            if (existingNotification != null)
+            {
+                _context.Notifications.Remove(existingNotification);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<List<Notification>> GetAllAsync()
+        {
+            return await _context.Notifications.ToListAsync();
+        }
+
+        public async Task<Notification?> GetByIdAsync(int id)
+        {
+            return await _context.Notifications.FindAsync(id);
+        }
+
+        public async Task<Notification?> UpdateAsync(Notification notification)
+        {
+            var existingNotification = await _context.Notifications.FindAsync(notification.Id);
+            if (existingNotification != null)
+            {
+                _context.Entry(existingNotification).CurrentValues.SetValues(notification);
+                await _context.SaveChangesAsync();
+                return notification;
+            }
+            return null;
+        }
+    }
+}
