733902c45bdf73060efcd031eaeee122e34fdd74
Forhad1286147 <forhad121398@gmail.com>
2026-08-21 23:52:49 +0600

    ITicketCommentRipositotory

---
+++ backend/src/Support_Ticket_Management_System_Application/Common/Interfaces/IRepositories/ITicketCommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using Support_Ticket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Support_Ticket.Application.Common.Interfaces.IRepositories
+{
+    public interface ITicketCommentRepository
+    {
+        Task<List<TicketComment>>GetAllAsync();
+        Task<TicketComment>GetByIdAsync(int id);
+        Task<TicketComment>AddAsync(TicketComment ticketComment);
+        Task<TicketComment>UpdateAsync(TicketComment ticketComment);
+        Task<bool>DeleteAsync(int id);
+
+    }
+}
