788cf5092b2af3df15e8ba71d5f74c0222f131c5
Forhad1286147 <forhad121398@gmail.com>
2026-08-22 21:26:55 +0600

    IdentityUserRepository

---
+++ backend/src/Support_Ticket_Management_System_Application/Common/Interfaces/IRepositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Support_Ticket.Application.Common.Interfaces.IRepositories
+{
+    public interface IUserRepository
+    {
+        Task<List<IdentityUser>> GetAllAsync();
+        Task<IdentityUser> GetByIdAsync(string id);
+        Task<IdentityUser> AddAsync(IdentityUser user);
+        Task<IdentityUser> UpdateAsync(IdentityUser user);
+        Task<bool> DeleteAsync(string id);
+    }
+}
---
+++ backend/src/Support_Ticket_Management_System_Infrastucture/Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Support_Ticket.Application.Common.Interfaces.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Support_Ticket.Infrastucture.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public UserRepository(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityUser> AddAsync(IdentityUser user)
+        {
+            await _userManager.CreateAsync(user);
+            return user;
+        }
+
+        public async Task<bool> DeleteAsync(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return false;
+
+            await _userManager.DeleteAsync(user);
+            return true;
+        }
+
+        public async Task<List<IdentityUser>> GetAllAsync()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+
+        public async Task<IdentityUser> GetByIdAsync(string id)
+        {
+            return await _userManager.FindByIdAsync(id);
+        }
+
+        public async Task<IdentityUser> UpdateAsync(IdentityUser user)
+        {
+            await _userManager.UpdateAsync(user);
+            return user;
+        }
+        }
+    }
+}
