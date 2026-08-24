abfaa2aaa47c15e2145ce409108f6fcc84126ab3
Forhad1286147 <forhad121398@gmail.com>
2026-08-24 21:04:39 +0600

    UserControllerGet

---
+++ backend/src/Support_Ticket_Management_System_Api/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Support_Ticket.Application.Services;
+
+namespace Support_Ticket.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAllAscyn();
+            return Ok(users);
+        }
+        [HttpGet("GetById")]
+        public async Task<IActionResult> GetByIdAsync(string id)
+        {
+            var user = await _userService.GetByIdAsycn(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+    }
+}
